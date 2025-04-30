e89a2190883554ab3ce7ee024b4a90be4f2d651b
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-04-30 11:31:07 +0200

    refactor: bucket policy

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,13 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "s3_web" {
   bucket              = "lostindusk.com"
   object_lock_enabled = false
+
   tags = {
     "application" = "lostindusk"
   }
+
   tags_all = {
     "application" = "lostindusk"
   }
@@ -11,6 +15,7 @@ resource "aws_s3_bucket" "s3_web" {
 
 resource "aws_s3_bucket_versioning" "s3_web_versioning" {
   bucket = aws_s3_bucket.s3_web.id
+
   versioning_configuration {
     status = "Enabled"
   }
@@ -27,6 +32,7 @@ resource "aws_s3_bucket_public_access_block" "s3_web_public_access_block" {
 
 resource "aws_s3_bucket_ownership_controls" "s3_web_ownership_controls" {
   bucket = aws_s3_bucket.s3_web.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -37,4 +43,33 @@ resource "aws_s3_bucket_acl" "s3_web_acl" {
 
   bucket = aws_s3_bucket.s3_web.id
   acl    = "private"
+}
+
+data "aws_iam_policy_document" "allow_cloudfront_access" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipal"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.s3_web.arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.cloudfront_distribution.id}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_web_policy" {
+  bucket = aws_s3_bucket.s3_web.id
+  policy = data.aws_iam_policy_document.allow_cloudfront_access.json
 }
