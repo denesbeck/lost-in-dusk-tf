958def75e5033ef43d301e297f41e68b0ae70f55
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-06-13 22:47:03 +0200

    fix: lambda import

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,15 @@
 resource "aws_lambda_function" "lostindusk_contact" {
   function_name = "LostInDuskContact"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "index.handler"
+  handler       = "PLACEHOLDER"
   runtime       = "nodejs22.x"
-  filename      = "function.zip"
+  filename      = "PLACEHOLDER.zip"
   lifecycle {
-    ignore_changes = [filename]
+    ignore_changes = all
   }
+}
+
+import {
+  to = aws_lambda_function.lostindusk_contact
+  id = "LostInDuskContact"
 }
